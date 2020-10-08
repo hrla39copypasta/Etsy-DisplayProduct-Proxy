fe7956579bb09cdcb1b61cfa6ca72ab2d9c17b9e
alphinaa <alphina.long@gmail.com>
2020-10-07 21:58:51 -0700

    Added all 5 modules together for the first time!

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+//Navbar endpoints
+app.use('/user', createProxyMiddleware({ target: 'http://localhost:8001', changeOrigin: true }));
+app.use('/entry', createProxyMiddleware({ target: 'http://localhost:8001', changeOrigin: true }));
+//displayproduct
+app.use('/product-display', createProxyMiddleware({ target: 'http://localhost:8002', changeOrigin: true }));
+app.use('/photo-display', createProxyMiddleware({ target: 'http://localhost:8002', changeOrigin: true }));
+//Reviews
+app.use('/reviewsItem', createProxyMiddleware({ target: 'http://localhost:8003', changeOrigin: true }));
+app.use('/reviewsShop', createProxyMiddleware({ target: 'http://localhost:8003', changeOrigin: true }));
+//Seller
+app.use('/api', createProxyMiddleware({ target: 'http://localhost:8004', changeOrigin: true }));
+//Related
+app.use('/related', createProxyMiddleware({ target: 'http://localhost:8005', changeOrigin: true }));
+app.listen(3000);
